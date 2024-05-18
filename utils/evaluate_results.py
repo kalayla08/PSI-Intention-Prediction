160d53466c014a5cdfe943a15feca819c4c46d42
kalayla08 <132407800+kalayla08@users.noreply.github.com>
2024-05-18 16:18:40 +0100

    Update evaluate_results.py

--- utils/evaluate_results.py
+++ utils/evaluate_results.py
@@ -1,6 +1,7 @@
 import json
-from sklearn.metrics import mean_squared_error, confusion_matrix, classification_report, accuracy_score, f1_score
+from sklearn.metrics import mean_squared_error, confusion_matrix, classification_report, accuracy_score, f1_score, precision_score, recall_score
 import numpy as np
+from tabulate import tabulate
 
 def evaluate_intent(groundtruth='', prediction='', args=None):
     with open(groundtruth, 'r') as f:
@@ -19,47 +20,76 @@ def evaluate_intent(groundtruth='', prediction='', args=None):
     gt = np.array(gt)
     pred = np.array(pred)
     res = measure_intent_prediction(gt, pred, args)
+    print('MSE: ',res['MSE'])
     print('Acc: ', res['Acc'])
     print('F1: ', res['F1'])
+    print('Precision: ', res['Precision'])
+    print('Recall: ', res['Recall'])
     print('mAcc: ', res['mAcc'])
-    print('ConfusionMatrix: ', res['ConfusionMatrix'])
-    return res['F1']
+    print('ConfusionMatrix: ')
+    print_confusion_matrix(res['ConfusionMatrix'], args)
+    return res['F1'], res['Precision'], res['Recall']
 
 
 
 def measure_intent_prediction(target, prediction, args):
     print("Evaluating Intent ...")
     results = {
+        #'MSE': 0,
         'Acc': 0,
         'F1': 0,
+        'Precision': 0,
+        'Recall': 0,
         'mAcc': 0,
         'ConfusionMatrix': [[]],
     }
 
     bs = target.shape[0]
+    lbl_target = np.argmax(target, axis=-1) # bs x ts
     lbl_target = target # bs
+    lbl_target_prob = target_prob
     lbl_pred = np.round(prediction) # bs, use 0.5 as threshold
 
-    # hard label evaluation - acc, f1
+    
+
+    MSE = np.mean(np.square(lbl_target_prob - prediction))
+
+    # Hard label evaluation - acc, f1, precision, recall
     Acc = accuracy_score(lbl_target, lbl_pred) # calculate acc for all samples
     F1_score = f1_score(lbl_target, lbl_pred, average='macro')
+    Precision = precision_score(lbl_target, lbl_pred, average='macro', zero_division=1)
+    Recall = recall_score(lbl_target, lbl_pred, average='macro')
 
-    intent_matrix = confusion_matrix(lbl_target, lbl_pred)  # [2 x 2]
+    if args.intent_num == 3:
+        intent_matrix = confusion_matrix(lbl_target, lbl_pred, labels=[0, 1, 2])  # [3 x 3]
+    else:
+        intent_matrix = confusion_matrix(lbl_target, lbl_pred)  # [2 x 2]
+
+
+    
     intent_cls_acc = np.array(intent_matrix.diagonal() / intent_matrix.sum(axis=-1)) # 2
     intent_cls_mean_acc = intent_cls_acc.mean(axis=0)
-
-    # results['MSE'] = MSE
+    
+    results['MSE'] = MSE
     results['Acc'] = Acc
     results['F1'] = F1_score
+    results['Precision'] = Precision
+    results['Recall'] = Recall
     results['mAcc'] = intent_cls_mean_acc
     results['ConfusionMatrix'] = intent_matrix
     return results
 
+def print_confusion_matrix(conf_matrix, args):
+    class_names = None
+    if args.intent_num == 2:
+        class_names = ['not_cross', 'cross']
+    elif args.intent_num == 3:
+        class_names = ['not_cross', 'not_sure', 'cross']
 
-if __name__ == '__main__':
-    args = None
-    # evaluate_intent('gt.json', 'pred.json', args)
-    test_gt_file = './val_intent_gt.json'
-    test_pred_file = './val_intent_prediction.json'
-    score = evaluate_intent(test_gt_file, test_pred_file, args)
-    print("Rankding score is : ", score)
+    headers = [""] + class_names
+    table = []
+    for i, row in enumerate(conf_matrix):
+        table.append([class_names[i]] + row.tolist())
+
+    print(tabulate(table, headers=headers, tablefmt="grid"))
+
