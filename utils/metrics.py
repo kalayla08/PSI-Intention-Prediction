a7d2be0dca1681b33d2923ca914b182faa183fe9
kalayla08 <132407800+kalayla08@users.noreply.github.com>
2024-05-26 20:45:06 +0100

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import mean_squared_error, confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
 import numpy as np
 
-def evaluate_intent(target, target_prob, prediction, args):
+def evaluate_intent(target, target_prob, prediction, args):  
     print("Evaluating Intent ...")
     results = {
         'MSE': 0,
@@ -9,29 +9,19 @@ def evaluate_intent(target, target_prob, prediction, args):
         'F1': 0,
         'Precision': 0,
         'Recall': 0,
-        'mAcc': 0,
         'ConfusionMatrix': [[]],
     }
-
-    target = np.array(target)
-    target_prob = np.array(target_prob)
-    prediction = np.array(prediction)
-
+    
+    bs = target.shape[0]
+    lbl_target = target # bs
+    lbl_target_prob = target_prob
+    
     if args.intent_num == 3:
-        # Convert target to one-hot encoding if necessary
-        if target.ndim == 1:
-            target_one_hot = np.zeros((target.size, prediction.shape[1]))
-            target_one_hot[np.arange(target.size), target] = 1
-        else:
-            target_one_hot = target
+        lbl_pred = np.argmax(prediction, axis=1) # Convert multilabel-indicator to multiclass
+    else: 
+        lbl_pred = np.round(prediction)
 
-        lbl_pred = np.argmax(prediction, axis=1)
-        lbl_target = np.argmax(target_one_hot, axis=1)
-    else:
-        lbl_pred = np.round(prediction).astype(int)
-        lbl_target = target.astype(int)
-
-    MSE = mean_squared_error(target_prob, prediction)
+    MSE = np.mean(np.square(lbl_target_prob - prediction))
     Acc = accuracy_score(lbl_target, lbl_pred)
     F1 = f1_score(lbl_target, lbl_pred, average='macro')
     Precision = precision_score(lbl_target, lbl_pred, average='macro', zero_division=1)
@@ -42,19 +32,15 @@ def evaluate_intent(target, target_prob, prediction, args):
     else:
         intent_matrix = confusion_matrix(lbl_target, lbl_pred)
 
-    intent_cls_acc = np.array(intent_matrix.diagonal() / intent_matrix.sum(axis=-1))
-    intent_cls_mean_acc = intent_cls_acc.mean(axis=0)
 
     results['MSE'] = MSE
     results['Acc'] = Acc
     results['F1'] = F1
     results['Precision'] = Precision
     results['Recall'] = Recall
-    results['mAcc'] = intent_cls_mean_acc
     results['ConfusionMatrix'] = intent_matrix
 
     return results
-
 
 def shannon(data):
     """
